2e916bf9de18b2685f3223b01af2e579c0f1ccf6
pragyan-satapathy <pragyans248@gmail.com>
2022-01-14 09:53:23 +0530

    '4'

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ print('outside function')
 
 
 def lambda_handler(event, context):
-    import a
+    # import a
     d = {'col1': [1, 2], 'col2': [3, 4]}
     df = pd.DataFrame(data=d)
     print(df)
