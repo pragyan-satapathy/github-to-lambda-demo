9ea514b3336fbd1f346a3e39d7b22e16466f83be
pragyan-satapathy <pragyans248@gmail.com>
2022-01-14 12:13:02 +0530

    'server-added'

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,20 @@
+from flask import Flask
 import pandas as pd
 
 print('outside function')
 
+app = Flask(__name__)
+
+
+@app.route('/home')
+def printHi():
+    return 'hello'
+
+
+app.run()
+
 
 def lambda_handler(event, context):
-    # import a
     d = {'col1': [1, 2], 'col2': [3, 4]}
     df = pd.DataFrame(data=d)
     print(df)
